75888095058550924764b1bb9018b03b71d2cde9
Matheus Victor <hffmatheus@gmail.com>
2016-09-07 11:07:49 -0300

    package.json dependencies updated

--- templates/common/root/_Gruntfile.js
+++ templates/common/root/_Gruntfile.js
@@ -380,7 +380,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          src: ['**/*.{png,jpg,jpeg,gif}'],
           dest: '<%%= yeoman.dist %><% if (express) { %>/public<% } %>/images'
         }]
       }
